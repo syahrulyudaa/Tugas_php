dbae2c97d3c19c5bed6659d3da9d65305e2d6ba9
Syahrul yuda <90370914+syahrulyudaa@users.noreply.github.com>
2023-04-29 12:23:04 +0700

    Update trapesium.php

--- bangun_datar/trapesium.php
+++ bangun_datar/trapesium.php
@@ -6,3 +6,4 @@ $sisi_miring = sqrt(pow(($sisi_b - $sisi_a) / 2, 2) + pow($tinggi, 2));
 $luas = (($sisi_a + $sisi_b) / 2) * $tinggi;
 
 echo "Luas trapesium dengan sisi atas $sisi_a, sisi bawah $sisi_b, dan tinggi $tinggi adalah $luas satuan luas.";
+?>
