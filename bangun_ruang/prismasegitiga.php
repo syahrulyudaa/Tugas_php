9095504a76cbf3369f98b7e44c6231b9d2de52a9
Syahrul yuda <90370914+syahrulyudaa@users.noreply.github.com>
2023-04-29 12:26:38 +0700

    Update prismasegitiga.php

--- bangun_ruang/prismasegitiga.php
+++ bangun_ruang/prismasegitiga.php
@@ -7,3 +7,4 @@ $luas_alas = 0.5 * $alas * $tinggi_alas;
 $luas_permukaan = 2 * $luas_alas + $alas * $tinggi_prisma + 3 * $tinggi_alas * $tinggi_prisma;
 
 echo "Luas permukaan prisma segitiga dengan alas $alas, tinggi alas $tinggi_alas, dan tinggi prisma $tinggi_prisma adalah $luas_permukaan satuan luas.";
+?>
