9e6daabc544c97477ad7badc0eb72350e321d60a
Syahrul yuda <90370914+syahrulyudaa@users.noreply.github.com>
2023-04-29 12:27:06 +0700

    Update tabung.php

--- bangun_ruang/tabung.php
+++ bangun_ruang/tabung.php
@@ -5,3 +5,4 @@ $tinggi = 10;
 $luas_permukaan = 2 * pi() * $jari_jari * ($jari_jari + $tinggi); 
 
 echo "Luas permukaan tabung dengan jari-jari $jari_jari dan tinggi $tinggi adalah $luas_permukaan satuan luas.";
+?>
