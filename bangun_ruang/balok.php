d952aeaf7c4854c6873f81a81c8a13312ba2b5b7
Syahrul yuda <90370914+syahrulyudaa@users.noreply.github.com>
2023-04-29 12:24:10 +0700

    Update balok.php

--- bangun_ruang/balok.php
+++ bangun_ruang/balok.php
@@ -6,3 +6,4 @@ $tinggi = 5;
 $luas_permukaan = 2 * (($panjang * $lebar) + ($panjang * $tinggi) + ($lebar * $tinggi)); // menghitung luas permukaan balok
 
 echo "Luas permukaan balok dengan panjang $panjang, lebar $lebar, dan tinggi $tinggi adalah $luas_permukaan satuan luas.";
+?>
